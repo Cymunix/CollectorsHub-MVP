1d3785c7ead937e3d77e1d2da156513178b7ed8b
Cymunix <hayden.macivor@gmail.com>
2026-03-12 16:35:23 -0300

    Fix redirect loop by using relative paths

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
   url: "https://grkeujhtaxmyjduzxoue.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdya2V1amh0YXhteWpkdXp4b3VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjcyMjksImV4cCI6MjA4ODg0MzIyOX0.xIx6rQ93bFgGoJmfx6u1fe4NLe1T-NhKer9fP_t0sBk",
-  postLoginRedirectPath: "/store.html"
+  postLoginRedirectPath: "store.html"
 };
